641eaefa4a2edef0edad143ab86c4148eda77603
teodorpaduraru <teodorpaduraru0@gmail.com>
2021-05-20 09:19:06 +0300

    1

---
+++ scripts/intern.js
@@ -0,0 +1,57 @@
+﻿var intern = {
+    content: {
+        'en': 'SuperHeroes Web Game (short: SuH) is an educational web game for teens.\nThe game contains a scenario, a stake and the default victory / defeat conditions for each round.\nFor a more pleasant and much more attractive interaction, all the games played will be of the trivia type (conquistador).\nThank you for choosing our game and we wish you a unique experience!\nTeam: Teodor Păduraru and Eugen Gavrilovici',
+        'ro': 'SuperHeroes Web Game (pe scurt: SuH) este un joc Web educativ destinat adolescenților.\nJocul conține un scenariu, o miză și condițiile de victorie/ înfrângere prestabilite pentru fiecare rundă.\nPentru o interacțiune mai plăcută și mult mai atractivă, toate partidele jucate vor fi de tipul trivia(conquistador).\nVă mulțumim că ați ales jocul nostru și vă dorim o experiență inedită!\nEchipa: Teodor Păduraru și Eugen Gavrilovici'
+    },
+
+    rules_content: {
+        'en': 'Rules\n\nYou can read the complete regulations and the user guide here: PDF or HTML.',
+        'ro': 'Reguli\n\nRegulamentul complet și ghidul de utilizare îl puteți citi aici: PDF sau HTML.'
+    },
+
+    rules_container: {
+        'en': 'All quiz questions are required.\nEach correct answer is worth 5 points.\nWrong answers are penalized with -3 points.\nAfter sending the answers, it is NOT possible to return to the respective quiz.\nThe "Prev" / "Next" buttons are used to switch from one question to another.\nThe result of the quiz is displayed after "Submit".',
+        'ro': 'Toate întrebările quiz-urilor sunt obligatorii.\nFiecare răspuns corect valorează 5 puncte.\nRăspunsurile greșite sunt penalizate cu -3 puncte.\nDupă trimiterea răspunsurilor NU se mai poate reveni la quiz-ul respectiv.\nButoanele "Prev" / "Next" sunt folosite pentru a trece de la o întrebare la alta.\nRezultatul quiz-ului este afișat după "Submit".'
+    },
+
+    suh_columns: {
+        'en': 'Phone:\nMail to:\n\nIf you have problems with the game, please send an e-mail to the above address.\nIf you do not receive a quick response, please call the number shown to be assisted by one of our colleagues.\n\nOperating Hours:\n Monday - Friday: 9 AM - 5 PM\nSaturday: 10 AM - 2 PM\nSunday: Closed',
+        'ro': 'Telefon:\nTrimiteti la:\n\nDacă aveți probleme cu jocul, vă rugăm să trimiteți un email la adresa de mai sus.\nÎn cazul în care nu primiți un răspuns rapid, vă rugăm să sunați la numărul afișat pentru a fi ajutat de unul dintre colegii noștri.\n\nProgram lucru operatori:\nLuni - Vineri: 9 AM - 5 PM\nSâmbătă: 10 AM - 2 PM\nDuminică: Închis.'
+    },
+
+    abstract: {
+        'en': 'Username:',
+        'ro': 'Nume utilizator:'
+    },
+
+    row: {
+        'en': 'Profile details\n\nLevel:\nExperience:\nVictories/Defeats:\nCountry:\City:',
+        'ro': 'Detalii profil\n\nNivel:\nExperiență:\nVictorii/Înfrângeri:\nȚară:\nLocalitate:'
+    },
+
+    container: {
+        'en': 'Rank\nName\nPoints',
+        'ro': 'Rang\nNume\nPuncte'
+    },
+
+    signin_container:{
+        'en': 'Register and join your Superheroes!',
+        'ro': 'Înregistrați-vă și alăturați-vă supereroilor!'
+    },
+
+    signin_form:{
+        'en': 'Email:\nUsername:\nPassword',
+        'ro': 'Email:\nNume utilizator:\nParolă:'
+    },
+
+    login_register: {
+        'en': 'Already have an account\nForgot Password?',
+        'ro': 'Ai deja un cont\n Ai uitat parola?'
+    },
+
+    alternative_register: {
+        'en': 'Or register and LogIn with:',
+        'ro': 'Sau înregistrați-vă și conectați-vă cu:'
+    }
+
+}
