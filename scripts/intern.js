f1a4241f6cd9e113516d96352d7f2aee5407a3e6
teodorpaduraru <teodorpaduraru0@gmail.com>
2021-05-23 18:53:39 +0300

    Control Panel & Alte modificari

--- scripts/intern.js
+++ scripts/intern.js
@@ -1,57 +1,87 @@
 ﻿var intern = {
+
+
+    topnav: {
+        'en': <p>'Home',<\p><p>'LogIn',<\p><p>'Play!',<\p><p>'Gallery',<\p><p>'Ranking',<\p><p>'Profile',<\p><p>'Contact',<\p><p>'About',<\p><p>'Rules',<\p>
+        'ro': <p>'Acasă'<\p><p>'Logare'<\p><p>'Joaca!'<\p><p>'Galerie'<\p><p>'Clasament'<\p><p>'Profil'<\p>p>'Contact'<\p><p>'Despre'<\p><p>'Reguli'<\p>
+    }
+
+
+
+    home_page: {
+        'en': <p>'SuperHeroes Web Game: Educational web game for teenagers.<\p><p>The scenario, stake and conditions of victory / defeat are already predetermined.<\p><p>All games will take place in a trivia system (conquistador).<\p><p>Thanks and good luck to all!',<\p>
+        'ro': <p>'SuperHeroes Web Game: joc Web educativ destinat adolescenților.<\p><p>Scenariul, miza și condițiile de victorie/înfrângere sunt deja prestabilite.<\p><p>Toate partidele se vor desfășura în sistem trivia (conquistador).<\p><p>Mulțumim și succes tuturor!'<\p>
+    }
+
+    login_page: {
+        'en': <p>'Register and join your Superheroes!',<\p><p>'Username:',<\p><p>'Password',<\p><p>'Create an account',<\p><p>'Forgot Password?',<\p><p>'Or register and LogIn with:',<\p>
+        'ro': <p>'Înregistrați-vă și alăturați-vă supereroilor!'<\p><p>'Nume de utilizator'<\p><p>'Parola'<\p><p>'Creează un cont'<\p><p>'Ați uitat parola?'<\p><p>'Sau înregistrați-vă și conectați-vă cu:'<\p>
+    }
+
+
+    forgot_password: {
+        'en': <p>'Recover your Superhero account!',</p><p>'Email  or username:',<\p><p>'Send Recovery Email',<\p><p>'Already have an account',<\p><p>'Forgot Password?',<\p><p>'Or register and LogIn with:',<\p>
+        'ro': <p>'Înregistrați-vă și alăturați-vă supereroilor!'<\p><p>'Adresă de e-mail sau nume de utilizator:'<\p><p>'Trimiteți un e-mail de recuperare'<\p><p>'Ai deja un cont'<\p><p>'Ați uitat parola?'<\p><p>'Sau înregistrați-vă și conectați-vă cu:'<\p>
+    }
+
+
+
+
+
+
     content: {
-        'en': 'SuperHeroes Web Game (short: SuH) is an educational web game for teens.\nThe game contains a scenario, a stake and the default victory / defeat conditions for each round.\nFor a more pleasant and much more attractive interaction, all the games played will be of the trivia type (conquistador).\nThank you for choosing our game and we wish you a unique experience!\nTeam: Teodor Păduraru and Eugen Gavrilovici',
-        'ro': 'SuperHeroes Web Game (pe scurt: SuH) este un joc Web educativ destinat adolescenților.\nJocul conține un scenariu, o miză și condițiile de victorie/ înfrângere prestabilite pentru fiecare rundă.\nPentru o interacțiune mai plăcută și mult mai atractivă, toate partidele jucate vor fi de tipul trivia(conquistador).\nVă mulțumim că ați ales jocul nostru și vă dorim o experiență inedită!\nEchipa: Teodor Păduraru și Eugen Gavrilovici'
+        'en': <p>'SuperHeroes Web Game (short: SuH) is an educational web game for teens.<\p><p>The game contains a scenario, a stake and the default victory / defeat conditions for each round.<\p>For a more pleasant and much more attractive interaction, all the games played will be of the trivia type (conquistador).<\p><p>Thank you for choosing our game and we wish you a unique experience!<\p><p>Team: Teodor Păduraru and Eugen Gavrilovici',<\p>
+        'ro': <p>'SuperHeroes Web Game (pe scurt: SuH) este un joc Web educativ destinat adolescenților.<\p><p>Jocul conține un scenariu, o miză și condițiile de victorie/ înfrângere prestabilite pentru fiecare rundă.<\p>Pentru o interacțiune mai plăcută și mult mai atractivă, toate partidele jucate vor fi de tipul trivia(conquistador).<\p><p>Vă mulțumim că ați ales jocul nostru și vă dorim o experiență inedită!<\p><p>Echipa: Teodor Păduraru și Eugen Gavrilovici'<\p>
     },
 
     rules_content: {
-        'en': 'Rules\n\nYou can read the complete regulations and the user guide here: PDF or HTML.',
-        'ro': 'Reguli\n\nRegulamentul complet și ghidul de utilizare îl puteți citi aici: PDF sau HTML.'
+        'en': <p>'Rules<\p><p>You can read the complete regulations and the user guide here: PDF or HTML.',<\p>
+        'ro': <p>'Reguli<\p><p>Regulamentul complet și ghidul de utilizare îl puteți citi aici: PDF sau HTML.'<\p>
     },
 
     rules_container: {
-        'en': 'All quiz questions are required.\nEach correct answer is worth 5 points.\nWrong answers are penalized with -3 points.\nAfter sending the answers, it is NOT possible to return to the respective quiz.\nThe "Prev" / "Next" buttons are used to switch from one question to another.\nThe result of the quiz is displayed after "Submit".',
-        'ro': 'Toate întrebările quiz-urilor sunt obligatorii.\nFiecare răspuns corect valorează 5 puncte.\nRăspunsurile greșite sunt penalizate cu -3 puncte.\nDupă trimiterea răspunsurilor NU se mai poate reveni la quiz-ul respectiv.\nButoanele "Prev" / "Next" sunt folosite pentru a trece de la o întrebare la alta.\nRezultatul quiz-ului este afișat după "Submit".'
+        'en': <p>'All quiz questions are required.<\p><p>Each correct answer is worth 5 points.<\p><p>Wrong answers are penalized with -3 points.<\p><p>After sending the answers, it is NOT possible to return to the respective quiz.<\p><p>The "Prev" / "Next" buttons are used to switch from one question to another.<\p>The result of the quiz is displayed after "Submit".',<\p>
+        'ro': <p>'Toate întrebările quiz-urilor sunt obligatorii.<\p>Fiecare răspuns corect valorează 5 puncte.<\p>Răspunsurile greșite sunt penalizate cu -3 puncte.<\p>După trimiterea răspunsurilor NU se mai poate reveni la quiz-ul respectiv.<\p>Butoanele "Prev" / "Next" sunt folosite pentru a trece de la o întrebare la alta.<\p>Rezultatul quiz-ului este afișat după "Submit".'<\p>
     },
 
     suh_columns: {
-        'en': 'Phone:\nMail to:\n\nIf you have problems with the game, please send an e-mail to the above address.\nIf you do not receive a quick response, please call the number shown to be assisted by one of our colleagues.\n\nOperating Hours:\n Monday - Friday: 9 AM - 5 PM\nSaturday: 10 AM - 2 PM\nSunday: Closed',
-        'ro': 'Telefon:\nTrimiteti la:\n\nDacă aveți probleme cu jocul, vă rugăm să trimiteți un email la adresa de mai sus.\nÎn cazul în care nu primiți un răspuns rapid, vă rugăm să sunați la numărul afișat pentru a fi ajutat de unul dintre colegii noștri.\n\nProgram lucru operatori:\nLuni - Vineri: 9 AM - 5 PM\nSâmbătă: 10 AM - 2 PM\nDuminică: Închis.'
+        'en': <p>'Phone:<\p>Mail to:<\p><p>If you have problems with the game, please send an e-mail to the above address.<\p>If you do not receive a quick response, please call the number shown to be assisted by one of our colleagues.<\p><p>Operating Hours:<\p><p>Monday - Friday: 9 AM - 5 PM<\p><p>Saturday: 10 AM - 2 PM<\p><p>Sunday: Closed',<\p>
+        'ro': <p>'Telefon:<\p>Trimiteti la:<\p><p>Dacă aveți probleme cu jocul, vă rugăm să trimiteți un email la adresa de mai sus.<\p><p>În cazul în care nu primiți un răspuns rapid, vă rugăm să sunați la numărul afișat pentru a fi ajutat de unul dintre colegii noștri.<\p><p>Program lucru operatori:<\p><p>Luni - Vineri: 9 AM - 5 PM<\p><p>Sâmbătă: 10 AM - 2 PM<\p><p>Duminică: Închis.'<\p>
     },
 
     abstract: {
-        'en': 'Username:',
-        'ro': 'Nume utilizator:'
+        'en': <p>'Username:',<\p>
+        'ro': <p>'Nume utilizator:'<\p>
     },
 
     row: {
-        'en': 'Profile details\n\nLevel:\nExperience:\nVictories/Defeats:\nCountry:\City:',
-        'ro': 'Detalii profil\n\nNivel:\nExperiență:\nVictorii/Înfrângeri:\nȚară:\nLocalitate:'
+        'en': <p>'Profile details<\p><p>Level:<\p><p>Experience:<\p><p>Victories/Defeats:<\p><p>Country:\City:',<\p>
+        'ro': <p>'Detalii profil<\p><p>Nivel:<\p><p>Experiență:<\p><p>Victorii/<p>Înfrângeri:<\p><p>Țară:<\p><p>Localitate:'<\p>
     },
 
     container: {
-        'en': 'Rank\nName\nPoints',
-        'ro': 'Rang\nNume\nPuncte'
+        'en': <p>'Rank<\p>Name<\p>Points',<\p>
+        'ro': <p>'Rang<\p>Nume<\p>Puncte'<\p>
     },
 
     signin_container:{
-        'en': 'Register and join your Superheroes!',
-        'ro': 'Înregistrați-vă și alăturați-vă supereroilor!'
+        'en': <p>'Register and join your Superheroes!',<\p>
+        'ro': <p>'Înregistrați-vă și alăturați-vă supereroilor!'<\p>
     },
 
     signin_form:{
-        'en': 'Email:\nUsername:\nPassword',
-        'ro': 'Email:\nNume utilizator:\nParolă:'
+        'en': <p>'Email:<\p><p>Username:<\p><p>Password',<\p>
+        'ro': <p>'Email:<\p><p>Nume utilizator:<\p><p>Parolă:'<\p>
     },
 
     login_register: {
-        'en': 'Already have an account\nForgot Password?',
-        'ro': 'Ai deja un cont\n Ai uitat parola?'
+        'en': <p>'Already have an account<\p><p>Forgot Password?',<\p>
+        'ro': <p>'Ai deja un cont<\p><p>Ai uitat parola?'<\p>
     },
 
     alternative_register: {
-        'en': 'Or register and LogIn with:',
-        'ro': 'Sau înregistrați-vă și conectați-vă cu:'
+        'en': <p>'Or register and LogIn with:',<\p>
+        'ro': <p>'Sau înregistrați-vă și conectați-vă cu:'<\p>
     }
 
 }
